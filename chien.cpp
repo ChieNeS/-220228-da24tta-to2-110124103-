de05f9a6b596d1bd915896a4f837e8db562ee588
ChieNeS <duyk852@gmail.com>
2024-11-12 10:41:09 +0700

    Add files via upload

---
+++ chien.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main ()
+{  
+   printf("toi ten la chien \n");
+   printf("ma so sinh vien 110124013 \n");
+   printf("sinh nam 2006 \n");
+   printf("diem xet tuyen la 23 \n");
+  return 0;
+ }
+
