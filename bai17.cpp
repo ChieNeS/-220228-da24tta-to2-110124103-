36a2886e1a5b0f8da01ff4f5a7f033fdd5a32132
ChieNeS <duyk852@gmail.com>
2024-11-19 10:24:57 +0700

    Add files via upload

---
+++ bai17.cpp
@@ -0,0 +1,105 @@
+#include<stdio.h>
+
+
+void ingiatri()
+{
+	int n,a;
+	printf("nhap n:");
+	scanf("%d",&n);
+	
+	for(a=1;a<=n;a++)
+	{
+	printf("%d \n",a);
+	}
+}
+
+void ingiatri1()
+{
+	int n,a;
+	printf("nhap n:\n");
+	scanf("%d",&n);
+	
+	for(a=0;a<=n;a=a+2)
+	{
+	printf("%d \n",a);
+	}
+}
+void ingiatri2()
+{
+	int n,i;
+	printf("nhap n:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	if(i%5==0)
+	
+	{
+	printf("%d ",i);
+	}
+
+
+	
+
+
+void ingiatri4(int n)
+{
+	int s,a;
+	printf("nhap n:");
+	scanf("%d",&n);
+	for(a=1;a<=n;a=a++)
+	{
+	s=s+a;
+	}
+	printf(" %d  ",&s);	
+}
+	
+
+void ingiatri5(int n)
+{
+	int s,a;
+	printf("nhap n:");
+	scanf("%d",&n);
+	for(a=1;a<=n;a=a+2)
+	{
+	s=s+a;
+	}
+	printf(" %d  ",&s);	
+}
+	
+int main() 
+{
+	int n,s-0;
+	printf("nhap n:\n");
+	scanf("%d",n);
+	for(i=1;i<=n;i+1)
+	{
+	for(intj=2;j<=n;j+1)
+    { 
+	if(j%i!=0)
+	{
+	s=s+i
+	}
+ 	}
+	}
+	printf("s=%d,s");
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+	
+	tongcacsongto<n();
+}
+	
+	
